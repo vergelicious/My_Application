27a410ccec876d2bc310aec6850f37e67a5a4045
vergelicious <vergel945@gmail.com>
2020-11-06 23:45:13 +0800

    Done. Did not do other commit because of problem in data connection

--- app/src/main/java/com/example/myapplication/Application.kt
+++ app/src/main/java/com/example/myapplication/Application.kt
@@ -3,13 +3,58 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.Settings
 import android.widget.Button
+import android.widget.Toast
 
 class Application : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application)
 
+        findViewById<Button>(R.id.btnClock).setOnClickListener { openClock() }
+        findViewById<Button>(R.id.btnCalendar).setOnClickListener { openCalendar() }
+        findViewById<Button>(R.id.btnMessage).setOnClickListener { openMessages() }
+        findViewById<Button>(R.id.btnSettings).setOnClickListener { openSettings() }
+        findViewById<Button>(R.id.btnAlarm).setOnClickListener { openAlarm() }
+    }
 
+    private fun openClock() {
+        val intent = Intent(Intent.ACTION_QUICK_CLOCK)
+        startActivity(intent)
+    }
+
+    private fun openCalendar() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_CALENDAR)
+        startActivity(intent)
+    }
+
+    private fun openMessages() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+        startActivity(intent)
+    }
+
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_SETTINGS)
+        intent.addCategory(Intent.ACTION_ALL_APPS)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Unfortunately settings has stopped.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openAlarm() {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+        intent.addCategory(Intent.ACTION_ALL_APPS)
+        startActivity(intent)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Cannot open clock.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
